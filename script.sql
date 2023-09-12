9723d021572a910278ee9060e5b816db9a1c2b32
Aluno <aluno@IPIRANGA.FATEC>
2023-09-12 11:37:36 -0300

    ap09(selecao): funcao de teste de data
    - Pode melhorar

--- script.sql
+++ script.sql
@@ -1,20 +1,70 @@
---CASE
-DO $$
+--dada uma data no formato ddmmaaaa, verificar se ela é valida
+--testar
+DO 
+$$
 DECLARE
-	valor INT := fn_valor_aleatorio_entre(1, 12);
-	mensagem VARCHAR(200);
+	data INT := 29102022;
+	dia INT;
+	mes INT;
+	ano INT;
+	data_valida BOOL := TRUE;
 BEGIN
-	RAISE NOTICE 'O valor é %', valor;
-	CASE valor
-		WHEN 1, 3, 5, 7, 9 THEN
-			RAISE NOTICE 'Impar';
-		WHEN 2, 4, 6, 8 THEN
-			RAISE NOTICE 'Par';
+	dia := data / 1000000;
+	mes := data % 1000000 / 10000;
+	ano := data % 10000;
+	RAISE NOTICE 'A data é %/%/%', dia, mes, ano;
+	IF ano < 1 THEN
+		RAISE NOTICE 'Data inválida';
+	ELSE
+		CASE
+			WHEN mes > 12 OR mes < 12 OR dia < 1 OR dia > 31 THEN
+				data_valida := FALSE;
+			ELSE
+				IF ((mes = 4 OR mes = 6 OR mes = 9 OR mes = 11) AND dia > 30) THEN
+				data_valida := FALSE;
+				ELSE
+					IF mes = 2 THEN
+						CASE
+							WHEN (ano % 4 = 0 AND ano % 100 <> 0) THEN
+								IF dia > 29 THEN
+									data_valida := FALSE;
+								END IF;
+							ELSE
+								--fevereiro e não é bissexto
+								IF dia > 28 THEN
+									data_valida := FALSE;
+								END IF;
+						END CASE;
+					END IF;
+				END IF;
+		END CASE;
+	END IF;
+	CASE
+		WHEN data_valida THEN
+			RAISE NOTICE 'A data é valida';
 		ELSE
-			RAISE NOTICE 'Fora do intervalo de tratamento';
+			RAISE NOTICE 'A data é invalida';
 	END CASE;
 END;
 $$
+
+-- --CASE
+-- DO $$
+-- DECLARE
+-- 	valor INT := fn_valor_aleatorio_entre(1, 12);
+-- 	mensagem VARCHAR(200);
+-- BEGIN
+-- 	RAISE NOTICE 'O valor é %', valor;
+-- 	CASE valor
+-- 		WHEN 1, 3, 5, 7, 9 THEN
+-- 			RAISE NOTICE 'Impar';
+-- 		WHEN 2, 4, 6, 8 THEN
+-- 			RAISE NOTICE 'Par';
+-- 		ELSE
+-- 			RAISE NOTICE 'Fora do intervalo de tratamento';
+-- 	END CASE;
+-- END;
+-- $$
 
 -- DO $$
 -- DECLARE
